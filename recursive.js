b0bac1774510792d790d6825e11a500e29d24318
unknown <ADMIN@LP59.1Rintern.local>
2024-09-03 15:14:24 +0530

    recursive addition

--- recursive.js
+++ recursive.js
@@ -118,3 +118,15 @@ const power = (x, n) => {
 };
 
 // console.log(power(3, 3));
+
+/**
+ * 8. Checks if a string is a palindrome.
+ */
+
+function isPalindrome(str) {
+  if (str.length <= 1) return true;
+  if (str[0] !== str[str.length - 1]) return false;
+  return isPalindrome(str.slice(1, -1));
+}
+
+console.log(isPalindrome("racecar"));
